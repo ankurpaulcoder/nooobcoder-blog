29c2aa65f94500fa94c8edc105293eb1e757739e
nooobcoder <suryashi2013@gmail.com>
2022-05-27 03:55:22 +0000

    Customisations

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -8,10 +8,11 @@ import 'react-notion-x/src/styles.css'
 import 'katex/dist/katex.min.css'
 
 // used for code syntax highlighting (optional)
-import 'prismjs/themes/prism-coy.css'
+// import 'prismjs/themes/prism-coy.css'
 
 // this might be better for dark mode
-// import 'prismjs/themes/prism-okaidia.css'
+import '../styles/prism-night-owl.css'
+// import 'prismjs/themes/prism-tomorrow.css'
 
 // global style overrides for notion
 import 'styles/notion.css'
