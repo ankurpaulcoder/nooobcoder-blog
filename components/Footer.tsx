d990300c35dd9ae121d81b33edff099235ca765b
nooobcoder <suryashi2013@gmail.com>
2022-10-20 07:09:41 +0000

    Calculate current year and display in footer

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -29,9 +29,14 @@ export const FooterImpl: React.FC = () => {
     setHasMounted(true)
   }, [])
 
+  // Current year: 2022
+  const year = `${new Date().getFullYear()}`
+
   return (
     <footer className={styles.footer}>
-      <div className={styles.copyright}>Copyright 2022 {config.author}</div>
+      <div className={styles.copyright}>
+        Copyright {year} {config.author}
+      </div>
 
       <div className={styles.settings}>
         {hasMounted && (
